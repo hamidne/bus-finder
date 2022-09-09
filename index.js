5f156d4c71f50a8e639b29bf71022d5a90cdb498
HamidNE <hamidne@mail.ru>
2022-09-09 13:36:01 +0430

    fix: fixed hours

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import cron from "node-cron";
 import {checkHasAvailable, sendAvailableNotification} from "./utils.js";
 
 cron.schedule('*/2 * * * *', async () => {
-    const items = await checkHasAvailable('21310000', '11320000', '2022-09-09T22:00:00', [13, 24]);
+    const items = await checkHasAvailable('21310000', '11320000', '2022-09-09T22:00:00', [22, 24]);
     if (items.length) await sendAvailableNotification(items);
 });
